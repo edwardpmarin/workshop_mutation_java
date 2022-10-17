9fcefd268497211b3d0f7fd21c9f0420d775edb0
Edward Alexander Pineda Marin <edward.pinedam@perficient.com>
2022-10-17 18:54:26 -0500

    test: add mutation plugin add reame and edit test scenarios

--- src/test/java/com/example/demo/perficient/ContactDataJpaShould.java
+++ src/test/java/com/example/demo/perficient/ContactDataJpaShould.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-public class ContactDataJpaTest {
+public class ContactDataJpaShould {
 
     @Autowired
     TestEntityManager entityManager;
@@ -47,54 +47,80 @@ public class ContactDataJpaTest {
 
     @ExceptionHandler(ConstraintViolationException.class)
     @Test
-    public void getByIdCorrectValuesTest() {
+    public void getContact_When_GetByIdCorrectValues() {
+        // Given
         Contact contact_request_initial = new Contact("Dairo", "Quintero", "+57302336789", "dairo.test@gmail.com");
         Contact contact_save_response = contactService.save(contact_request_initial);
+        
+        // When
         Contact contact_response = contactService.getById(new Contact(contact_save_response.getId()));
+        
+        // Then
         assertThat(contact_response.getId()).isNotNull();
         assertThat(contact_response.getFirstName()).isEqualTo(contact_request_initial.getFirstName());
+        assertThat(contact_response.getLastName()).isEqualTo(contact_request_initial.getLastName());
         assertThat(contact_response.getEmail()).isEqualTo(contact_request_initial.getEmail());
     }
 
     @ExceptionHandler(InvalidDataAccessApiUsageException.class)
     @Test
-    public void getByIdMissindIdTest() {
+    public void getContact_When_GetByIdMissindId() {
+        // Given
         Contact contact_request_initial = new Contact("Dairo", "Quintero", "+57302336789", "dairo.test@gmail.com");
         contactService.save(contact_request_initial);
+
+        // When
         Contact contact_request = new Contact("Carlos");
+
+        // Then
         Exception exception = assertThrows(InvalidDataAccessApiUsageException.class, () -> contactService.getById(contact_request));
         assertThat(exception.getMessage()).contains("The given id must not be null!");
     }
 
     @ExceptionHandler(InvalidDataAccessApiUsageException.class)
     @Test
-    public void getByNameCorrectValuesTest() {
+    public void getContact_When_GetByNameCorrectValues() {
+        // Given
         Contact contact_request_initial = new Contact("Dairo", "Quintero", "+57302336789", "dairo.test@gmail.com");
         contactService.save(contact_request_initial);
         Contact contact_request = new Contact("Dairo");
+
+        // When
         List<Contact> contact_response = contactService.getByName(contact_request);
+
+        // Then
         assertThat(contact_response.get(0)).isNotNull();
         assertThat(contact_response.get(0).getFirstName()).isEqualTo(contact_request_initial.getFirstName());
     }
 
     @ExceptionHandler(InvalidDataAccessApiUsageException.class)
     @Test
-    public void getByNameNotMatchValuesTest() {
+    public void getContact_When_GetByNameNotMatchValues() {
+        // Given
         Contact contact_request_initial = new Contact("Dairo", "Quintero", "+57302336789", "dairo.test@gmail.com");
         contactService.save(contact_request_initial);
         Contact contact_request = new Contact("Pedro");
+
+        // When
         List<Contact> contact_response = contactService.getByName(contact_request);
+
+        // Then
         assertTrue(contact_response.isEmpty());
         assertThat(contact_response.size()).isEqualTo(0);
     }
 
     @ExceptionHandler(InvalidDataAccessApiUsageException.class)
     @Test
-    public void getByNameMissingNameTest() {
+    public void getContact_When_GetByNameMissingName() {
+        // Given
         Contact contact_request_initial = new Contact("Dairo", "Quintero", "+57302336789", "dairo.test@gmail.com");
         contactService.save(contact_request_initial);
         Contact contact_request = new Contact(5);
+
+        // When
         List<Contact> contact_response = contactService.getByName(contact_request);
+
+        // Then
         assertTrue(contact_response.isEmpty());
         assertThat(contact_response.size()).isEqualTo(0);
     }
@@ -102,17 +128,23 @@ public class ContactDataJpaTest {
 
     @ExceptionHandler(ConstraintViolationException.class)
     @Test
-    public void saveEmailInvalidTest() {
+    public void getContact_When_SaveEmailInvalid() {
+        // When
         Contact contact_request = new Contact("Dairo", "Quintero", "+57302336789", "dairo.testgmail.com");
         Exception exception = assertThrows(ConstraintViolationException.class, () -> contactService.save(contact_request));
+        
+        // Then
         assertThat(exception.getMessage()).contains("Email inválido");
     }
 
 
     @ExceptionHandler(ConstraintViolationException.class)
     @Test
-    public void saveFirstnameInvalidTestDeepTest() {
+    public void getContact_When_SaveFirstnameInvalidTestDeep() {
+        // Given
         Contact contact_request = new Contact("Da", "Quintero", "+57302336789", "dairo.test@gmail.com");
+        
+        // When
         ConstraintViolationException exception = assertThrows(ConstraintViolationException.class, () -> contactService.save(contact_request));
         Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
         Set<String> messages = new HashSet<>(constraintViolations.size());
@@ -121,27 +153,36 @@ public class ContactDataJpaTest {
                 //.map(constraintViolation -> String.format("%s value '%s' %s", constraintViolation.getPropertyPath(),
                 //        constraintViolation.getInvalidValue(), constraintViolation.getMessage()))
                 .map(constraintViolation -> String.format("%s", constraintViolation.getMessage())).collect(Collectors.toList()));
+        
+        // Then
         assertEquals("El nombre debe tener entre 3 y 30 caracteres", messages.iterator().next());
 
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @Test
-    public void saveFirstnameInvalidTestContainTest() {
+    public void getContact_When_SaveFirstnameInvalidTestContain() {
+        // When
         Contact contact_request = new Contact("Da", "Quintero", "+57302336789", "dairo.test@gmail.com");
         ConstraintViolationException exception = assertThrows(ConstraintViolationException.class, () -> contactService.save(contact_request));
+
+        // Then
         assertTrue(exception.getMessage().contains("El nombre debe tener entre 3 y 30 caracteres"));
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @Test
-    public void saveNumberInvalidTest() {
-
+    public void getContact_When_SaveNumberInvalid() {
+        // Given
         Contact contact_request = new Contact("Dairo", "Quintero", "4566990", "dairo.test@gmail.com");
+
+        // When
         ConstraintViolationException exception = assertThrows(ConstraintViolationException.class, () -> contactService.save(contact_request));
         Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
         Set<String> messages = new HashSet<>(constraintViolations.size());
         messages.addAll(constraintViolations.stream().map(constraintViolation -> String.format("%s", constraintViolation.getMessage())).collect(Collectors.toList()));
+        
+        // Then
         assertEquals("El número de telefono sólo puede tener dígitos iniciando con el símbolo +", messages.iterator().next());
     }
 
